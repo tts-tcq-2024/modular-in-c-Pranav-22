bf0e515f35abc27b36b4449764211681052dd18a
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-12 17:16:23 +0530

    Update and rename pair_color_code.h to tc_cable_pair_color_code.h

--- tc_cable_pair_color_code.h
+++ tc_cable_pair_color_code.h
@@ -1,5 +1,5 @@
-#ifndef PAIR_COLOR_CODE_H
-#define PAIR_COLOR_CODE_H
+#ifndef TC_CABLE_PAIR_COLOR_CODE_H
+#define TC_CABLE_PAIR_COLOR_CODE_H
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
@@ -10,5 +10,14 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
 
 #endif
