422f3cf3094bb3a2b9aa0bf201f49d1bcbe5e365
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-12 17:23:01 +0530

    Update color_code_unit_test.h

--- color_code_unit_test.h
+++ color_code_unit_test.h
@@ -1,6 +1,7 @@
 #ifndef COLOR_CODE_UNIT_TEST_H
 #define COLOR_CODE_UNIT_TEST_H
 
+#include "tc_cable_pair_color_code.h"
 
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
