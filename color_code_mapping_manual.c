f6df4381ef480923a5e464b21ca993cedf038ba5
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-17 10:32:43 +0530

    Update color_code_mapping_manual.c

--- color_code_mapping_manual.c
+++ color_code_mapping_manual.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include "tc_cable_pair_color_code.h"
 
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+
 void cable_pair_mapping_manual(map *map_manual)
 {
-  ColorPair colorPair;
+  ColorPair colorPair_manual;
   for(int i = 0 ; i<25 ;i++)
   {
-    colorPair = GetColorFromPairNumber(i+1);
+    colorPair_manual = GetColorFromPairNumber(i+1);
      map_manual[i].cable_num = i+1;
-     sprintf(map_manual[i].cable_major,"%s",MajorColorNames[colorPair.majorColor]);
-     sprintf(map_manual[i].cable_minor,"%s",MinorColorNames[colorPair.minorColor]);
+     sprintf(map_manual[i].cable_major,"%s",MajorColorNames[colorPair_manual.majorColor]);
+     sprintf(map_manual[i].cable_minor,"%s",MinorColorNames[colorPair_manual.minorColor]);
   }
 }
 
