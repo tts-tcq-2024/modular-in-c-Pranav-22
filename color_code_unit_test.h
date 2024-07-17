878bcbf1958b2c92223b80583e3f475f7d31c74d
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-17 10:27:23 +0530

    Update color_code_unit_test.h

--- color_code_unit_test.h
+++ color_code_unit_test.h
@@ -5,5 +5,6 @@
 
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+void testmanualpairprinting(void);
 
 #endif
