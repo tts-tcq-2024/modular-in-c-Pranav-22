630415651c969265ffa0676752ada74e1c523e79
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-17 10:25:39 +0530

    Update color_code_mapping_maual.c

--- color_code_mapping_maual.c
+++ color_code_mapping_maual.c
@@ -1,21 +1,21 @@
 #include "tc_cable_pair_color_code.h"
 
-typedef struct
+void cable_pair_mapping_manual(map *map_manual)
 {
-  int cable_num;
-  char cable_major[10];
-  char cable_minor[10];
-}map;
-
-void cable_pair_mapping_manual(void)
-{
-  map map_manual[25];
   ColorPair colorPair;
-  for(int i = 1 ; i<=25 ;i++)
+  for(int i = 0 ; i<25 ;i++)
   {
-    colorPair = GetColorFromPairNumber(i);
-    map_manual[i].cable_num = i;
-     sprintf(map_manual[i].cable_major,%s,MajorColorNames[colorPair->majorColor]);
-     sprintf(map_manual[i].cable_major,%s,MinorColorNames[colorPair->minorColor]);
+    colorPair = GetColorFromPairNumber(i+1);
+     map_manual[i].cable_num = i+1;
+     sprintf(map_manual[i].cable_major,"%s",MajorColorNames[colorPair.majorColor]);
+     sprintf(map_manual[i].cable_minor,"%s",MinorColorNames[colorPair.minorColor]);
   }
+}
+
+void cable_pair_manual_print(map *map_manual)
+{
+    for(int i=0;i<25;i++)
+    {
+        printf("%d  %s  %s\n",map_manual[i].cable_num,map_manual[i].cable_major,map_manual[i].cable_minor);
+    }
 }
