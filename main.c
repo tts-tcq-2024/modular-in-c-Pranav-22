317c9d7746f5955b8c8d5bade59a567277e9a4bb
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-17 10:28:22 +0530

    Update main.c

--- main.c
+++ main.c
@@ -41,8 +41,8 @@ int GetPairNumberFromColor(const ColorPair* colorPair) {
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+    testmanualpairprinting();
     return 0;
 }
